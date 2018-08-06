294624c139f634b38019ef9025e4c729e2a4aef8
Orosz <oroszandrei@gmail.com>
2018-08-06 20:35:49 +0300

    Added comments

--- app/src/main/java/com/orosz/myapp/audiospeech/MainActivity.java
+++ app/src/main/java/com/orosz/myapp/audiospeech/MainActivity.java
@@ -10,11 +10,15 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonTapped(View view) {
 
+        //id - view.getID() - gets the id of what you touched from the interior of the app
         int id = view.getId();
+        // ourID - where our id(the name we gave) is stored
         String ourID = "";
 
+        //the method below "translates" the id from the app to our particular id(that we gave) and maches it
         ourID = view.getResources().getResourceEntryName(id);
 
+        //gets the id for the audio file that we need
         int resourceID = getResources().getIdentifier(ourID, "raw", "com.orosz.myapp.audiospeech");
 
         MediaPlayer mPlayer = MediaPlayer.create(this, resourceID);
